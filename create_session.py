4687169a798375c6b6744330bc145a55e3f5606a
pbkgdubai <pbkgdubai@gmail.com>
2025-12-12 09:56:56 +0400

    Update create_session.py

--- create_session.py
+++ create_session.py
@@ -2,16 +2,17 @@ from telethon import TelegramClient
 import asyncio
 
 api_id = 38436869
-api_hash = "8ce925bc64c01445a7463faf4703a3c9"
+api_hash = '8ce925bc64c01445a7463faf4703a3c9'
 
-client = TelegramClient("pbkg_session", api_id, api_hash)
+client = TelegramClient('pbkg_session', api_id, api_hash)
 
 async def main():
-    await client.start()
-    print("Session created successfully!")
+    await client.start()  # при запуске спросит номер и код из Telegram
+    print("✅ Session создана!")
 
 with client:
     client.loop.run_until_complete(main())
-  python create_session.py
+
+
 
 
